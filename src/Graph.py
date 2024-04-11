4d2ad7573b5eeccc307b0078d4b8ef2251a65f64
Caporal Winnie <jacques.soghomonyan@efrei.net>
2024-04-11 09:46:09 +0200

    test graphs

--- src/Graph.py
+++ src/Graph.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from pprint import pprint
-from matplotlib.pyplot import margins
-from networkx import predecessor
+from typing import Any
 from tabulate import tabulate
 from rich import table
+
 class Graph:
     """
         The graph is represented using a dictionary with this form:
@@ -21,60 +21,78 @@ class Graph:
         }
     """ 
     
-    def __init__(self, number: int = 0, path_override: str = "") -> None:
+    def __init__(self, number: int = 0, path: str = "", string_override= False) -> None:
         
-        if (path_override == ""):
+        self.number = 'X'
+        if (string_override):
+            self.data = path # type: ignore
+        elif (path == ""):
             with (Path(__file__).parent.parent / Path(f"graphs/{number}.txt")).open() as f:
                 self.data: str = f.read()
             self.number = number
         else:
-            with Path(path_override).open() as f:
+            with Path(path).open() as f:
                 self.data: str = f.read()
-            self.number = 'X'
             
         
         _filter = lambda s: list(filter(lambda x: x, s))
         splitted_data: list[str] = _filter(self.data.split('\n'))
         
-        splitted_string: list[list[str]] = [_filter(string.split(' ')) for string in splitted_data]
+        splitted_string: list[list[int]] = [[int(i) for i in _filter(string.split(' '))] for string in splitted_data]
         
-        self.table: dict[str, dict[str, int]] = self._table_from_valid_list(splitted_string)          
-    
+        self.table: dict[int, dict[int, int]] = self.table_from_valid_list(splitted_string)          
         
-    def _table_from_valid_list(self, splitted_string: list[list[str]]) -> dict[str, dict[str, int]]:
+    def to_dot_format(self, highlight_critical_path=False):
+        to_dot = "digraph { rankdir=LR\n"
+        
+        crit: list = []
+        if highlight_critical_path:
+            
+            crit = self.get_critical_nodes()
+            for n in crit:
+                to_dot += f'    "{n}" [color="red"]\n'
+        
+        for node, succs in self.table.items():
+            for k, v in succs.items():
+                to_dot += f'    "{node}" -> "{k}" [label="{v}"{""" color="red" """ if node in crit and k in crit else """"""}] \n'
 
-        table: dict[str, dict[str, int]] = {}
+        to_dot += "}"
+        return to_dot
+        
+    def table_from_valid_list(self, splitted_string: list[list[int]]):
+
+        table: dict[int, dict[int, int]] = {}
                 
-        nodes: list[str] = [s[0] for s in splitted_string]
+        nodes: list[int] = [int(s[0]) for s in splitted_string]
         
         # initialization of the table
         for node in nodes:
             if node not in table.keys():
                 table[node] = {}
         
-        table['0'] = {}
+        table[0] = {}
         
         size: int = len(table.keys())
         
-        table[str(size)] = {}
+        table[size] = {}
        
         size += 1
         # We just added 2 to the size because of the the 2 new implicit states: alpha & omega
-        all_predecessors: list[str] = self._get_all_predecessors(splitted_string)
+        all_predecessors: list[int] = self.__get_all_predecessors(splitted_string)
         
         for line in splitted_string:
             
-            label: str = line[0]
-            predecessors: list[str] = line[2:]
+            label: int = int(line[0])
+            predecessors: list[int] = [int(i) for i in line[2:]]
             
             if not predecessors:
-                table['0'] |= {label: 0}
+                table[0] |= {label: 0}
                 continue
             
             if label not in all_predecessors:
                 # we subtract 2 because we added ɑ & ω
                 old_size: int = size-2
-                table[str(label)] = {str(size-1): int(splitted_string[old_size-1][1])}
+                table[label] = {size-1: int(splitted_string[label-1][1])}
             
             for node in predecessors:
                 table[node] |= {label: int(splitted_string[int(node)-1][1])}
@@ -82,8 +100,8 @@ class Graph:
         self.size: int = len(table.keys())
         return table
     
-    def _get_all_predecessors(self, splitted_string) -> list[str]:
-        predecessors: list[str] = []
+    def __get_all_predecessors(self, splitted_string) -> list[int]:
+        predecessors: list[int] = []
         
         for line in splitted_string:
             predecessors += line[2:]
@@ -102,21 +120,20 @@ class Graph:
         matrix = [[] for _ in range(length)]
         
         for k, v in self.table.items():
-            matrix[int(k)] = [1 if str(i) in v.keys() else 0 for i in range(length)]
+            matrix[int(k)] = [1 if i in v.keys() else 0 for i in range(length)]
             
-        return matrix
-    
+        return matrix  
     
     def get_value_matrix(self):
         length = len(self.table.keys())
         matrix = [[] for _ in range(length)]
         
         for k, v in self.table.items():
-            matrix[int(k)] = [v.get(str(i)) if str(i) in v.keys() else 0 for i in range(length)]
+            matrix[int(k)] = [v.get(i) if i in v.keys() else '' for i in range(length)]
             
         return matrix
     
-    def _get_predecessors(self, node):
+    def get_predecessors(self, node):
         
         if node not in self.table.keys():
             return []
@@ -127,16 +144,15 @@ class Graph:
                 predecessors.append(label)
                 
         return list(set(predecessors))
-                
-        
+                     
     def get_constraint_table(self):
         table = {}
         
         for label, successors in self.table.items():
             if time:=list(successors.values()):
-                table[label] = {time[0]: self._get_predecessors(label)}
+                table[label] = {time[0]: self.get_predecessors(label)}
             else:
-                table[label] = {0: self._get_predecessors(label)}
+                table[label] = {0: self.get_predecessors(label)}
                 
         return table
     
@@ -156,9 +172,9 @@ class Graph:
         steps = []
         
         #stupid hack for backwards compatibility, I AM FUCKING STUPID
-        constraint: list[list[str | list[str]]] = [[k] + [str(e) for e in v.keys()] + list(v.values()) for k,v in self.get_constraint_table().items()]
+        constraint: list[list[Any]] = [[k] + [e for e in v.keys()] + list(v.values()) for k,v in self.get_constraint_table().items()]
         
-        deleted_nodes: list[str] = []
+        deleted_nodes: list[int] = []
                 
         for _ in range(len(constraint)):
             is_impossible = 0
@@ -186,8 +202,8 @@ class Graph:
         if self.has_circuit_by_transitive():
             raise ValueError("The graph has a circuit !!!! get fucked loser")
         
-        constraint: list[list[str | list[str]]] = [[k] + [str(e) for e in v.keys()] + list(v.values()) for k,v in self.get_constraint_table().items()]
-        deleted_nodes: list[str] = []
+        constraint: list[list[Any]] = [[k] + [e for e in v.keys()] + list(v.values()) for k,v in self.get_constraint_table().items()]
+        deleted_nodes: list[int] = []
         ranks = {}
         
         current_rank = 0
@@ -195,7 +211,7 @@ class Graph:
                         
             for line in constraint:
                 
-                label: str = line[0] # type:ignore
+                label: int = line[0] # type:ignore
                 predecessors = line[2]
                 if predecessors:
                     continue
@@ -210,14 +226,14 @@ class Graph:
             
         return ranks
     
-    def _get_node_time(self, node):
+    def get_node_time(self, node):
         if not node in self.table.keys():
             raise ValueError("node not in graph")
         
         l = list(self.table[node].values())
         return l[0] if len(l) > 0 else 0
     
-    def _get_successors(self, node):
+    def get_successors(self, node):
         
         if node not in self.table.keys():
             return []
@@ -225,66 +241,37 @@ class Graph:
         return list(self.table[node].keys())
     
     def get_node_max_time(self, node):
-        predecessors = self._get_predecessors(node)
+        predecessors = self.get_predecessors(node)
         if not predecessors:
             return 0
         
-        return max([self.get_node_max_time(x) + self._get_node_time(x) for x in predecessors])
+        return max([self.get_node_max_time(x) + self.get_node_time(x) for x in predecessors])
 
     def get_node_min_time(self, node):
-        successors = self._get_successors(node)
+        successors = self.get_successors(node)
         if not successors:
             return self.get_node_max_time(node)
         
-        return min([self.get_node_min_time(x) - self._get_node_time(node) for x in successors])
+        return min([self.get_node_min_time(x) for x in successors]) - self.get_node_time(node)
         
-    def get_early_calendar(self):
+    def get_earliest_calendar(self):
+                       
+        return {n : self.get_node_max_time(n) for n in self.table.keys()}
         
-        ranks = self.get_ranks()
-               
-        return [self.get_node_max_time(n)for n in ranks.keys()]
+    def get_latest_calendar(self):
         
-    def get_late_calendar(self):
-        
-        ranks = self.get_ranks()
-        
-        return [self.get_node_min_time(n) for n in ranks.keys()]
-    
-    
+        return {n : self.get_node_min_time(n) for n in self.table.keys()}
+       
     def get_margins(self):
-        
-        ranks = self.get_ranks()
-        
-        early = [self.get_node_min_time(n) for n in ranks.keys()]
-        late = [self.get_node_max_time(n) for n in ranks.keys()]
-
-        return [a - b for a,b in zip(early, late)]
     
+        return {n : self.get_node_min_time(n) - self.get_node_max_time(n) for n in self.table}
     
-
-    # t = table.Table(title="Calendrier au plus tard", show_lines=True)
-    
-    # t.add_column("rank")
-    
-    # for e in ranks.values():
-    #     t.add_column(str(e))
-        
-    # t.add_row("node", *[f"{k}({self._get_node_time(k)})" for k in ranks.keys()])
-    # t.add_row("successors", *[','.join(self._get_successors(n)) for n in ranks.keys()])
-    # t.add_row("calendar", *)
-    
-    def get_critical_path(self):
-        
-        rank = self.get_ranks()
-        
+    def get_critical_nodes(self):        
         margins = self.get_margins()
         
+        c = dict(filter(lambda p: p[1] == 0, margins.items())).keys()
         
-        return []
-        
-        
-        
-        
+        return list(c)
         
     def __str__(self) -> str:
         
--- src/main.py
+++ src/main.py
@@ -3,30 +3,37 @@ from time import sleep
 from tabulate import tabulate
 from Graph import Graph
 from rich import print, table
+import graphviz
 
 graphs = [Graph(i) for i in range(1, 15)]
 
 
 i = 0
 
-# while (True):
-#     i += 1
-#     i = i % 14
-#     print(graphs[i])
-#     print(tabulate(graphs[i].get_value_matrix(), tablefmt="rounded_grid"))
+while (True):
+    # print(graphs[i])
+    # print(tabulate(graphs[i].get_value_matrix(), tablefmt="rounded_grid"))
     
-#     # print(graphs[i].has_negative_arcs())
-#     # print(graphs[i].has_circuit_by_transitive())
-#     if not graphs[i].has_circuit_by_transitive():
-#         print(graphs[i].get_ranks())  
-#         print(graphs[i].get_early_calendar())  
-#         print(graphs[i].get_late_calendar())  
+    # print(graphs[i].has_negative_arcs())
+    # print(graphs[i].has_circuit_by_transitive())
+    if not graphs[i].has_circuit_by_transitive():
+        graphviz.Source(graphs[i].to_dot_format(True)) \
+            .render(outfile=(f'out/{i+1}.png'), view=False, format="png")
+    else:
+        graphviz.Source(graphs[i].to_dot_format()) \
+            .render(outfile=(f'out/{i+1}.png'), view=False, format="png")
     
-#     sleep(0.5)
+    i += 1
+    if i == 14: break
+    # sleep(0.5)
 
 
 # print(Graph(path_override="./graphs/test_2.txt").get_ranks())
-print(Graph(path_override="./graphs/test_2.txt").table)
-print(Graph(path_override="./graphs/test_2.txt").get_early_calendar())
-print(Graph(path_override="./graphs/test_2.txt").get_late_calendar())
-print(Graph(path_override="./graphs/test_2.txt").get_margins())
+# print(Graph(path_override="./graphs/test_1.txt").table)
+# print(Graph(path_override="./graphs/test_2.txt").get_early_calendar())
+# print(Graph(path_override="./graphs/test_2.txt").get_late_calendar())
+# print(Graph(path_override="./graphs/test_2.txt").get_margins())
+# print(Graph(path_override="./graphs/test_2.txt").get_critical_path())
+# if not Graph(path="./graphs/test_1.txt").has_circuit_by_transitive():
+#     print(Graph(path="./graphs/test_2.txt").get_critical_nodes())
+
